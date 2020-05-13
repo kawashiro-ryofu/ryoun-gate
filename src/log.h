52888dfc2037f66cb17e5937d9c7d7317a59c325
网管38 <unsicencific_kappa@foxmail.com>
2020-05-14 00:05:31 +0800

    日志记录

--- src/log.h
+++ src/log.h
@@ -1,3 +1,11 @@
+/*
+	Copyright 2020 Jeffery_Yu
+    Licence Under MIT
+    
+    log.h
+    日志记录
+
+*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -8,18 +16,19 @@
 
 
 void log(int outsign,char outtext[]){
+  	//打开日志文件，末尾追加
 	FILE *log;
 	log = fopen("./ry.log","a+");
 	char *sign;
 	char *color;
 
-
+	//时间获取
 	struct tm *clock;
 	time_t now;
 	time(&now);
 	clock = localtime(&now);
 
-
+	//标识符及颜色
 	switch(outsign){
 		case 0:color = "\033[32;1m";sign = "[i]";break;
 		case 1:color = "\033[33;1m";sign = "<!>";break;
@@ -28,6 +37,7 @@ void log(int outsign,char outtext[]){
 	
 	}
 	
+  	//输出
 	printf("[%d:%d:%d]",clock->tm_hour,clock->tm_min,clock->tm_sec);
 	printf("%s%s\033[;0m%s\n",color,sign,outtext);
 	fprintf(log,"[%d-%d-%d %d:%d:%d]",clock->tm_year+1900,clock->tm_mon+1,clock->tm_mday,clock->tm_hour,clock->tm_min,clock->tm_sec);
